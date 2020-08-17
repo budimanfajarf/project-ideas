704b6a25a23db599ef41d825da9ed516d86d276e
budimanfajarf <budimanfajarf@gmail.com>
2020-08-17 20:49:36 +0700

    Success test a few GitHub API with Personal Access Token

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/test-github-api', function () {
+    $baseUrl = 'https://api.github.com';
+    $token = env('GITHUB_TOKEN');
+
+    // dd($token);
+
+    // $response = Http::withHeaders([
+        // 'Authorization' => "token {$token}",
+    // ])
+    // ->get("{$baseUrl}/rate_limit");
+
+    // repo info
+    // ->get("{$baseUrl}/repos/florinpop17/app-ideas");    
+
+    $owner = 'budimanfajarf';
+    $repo = 'app-ideas';
+    $path = 'Projects';
+    // $path = 'Projects/1-Beginner';    
+
+    // path info
+    $response = Http::withHeaders([
+        'Authorization' => "token {$token}",
+    ])
+    ->get("{$baseUrl}/repos/{$owner}/{$repo}/contents/${path}");            
+
+    return response()->json($response->json());
+});
 
 Route::get('/', function () {
     return view('welcome');
