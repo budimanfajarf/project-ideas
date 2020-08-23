67da8662022f30bd1e6e74d699d47535cc9d7870
budimanfajarf <budimanfajarf@gmail.com>
2020-08-23 18:29:54 +0700

    Add php artisan logs:clear

---
+++ app/Console/Commands/ClearLogs.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear log files';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // https://stackoverflow.com/questions/28127495/in-phps-laravel-how-do-i-clear-laravel-log
+        exec('rm ' . storage_path('logs/*.log'));
+        $this->comment('Logs have been cleared!');
+    }
+}
